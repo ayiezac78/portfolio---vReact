5b4c02168f819975a54deff465520c151b36f3a2
Ariel Maniago <admaniago01@gmail.com>
2024-11-24 11:38:13 +0800

    made some changes

---
+++ src/components/ProjectCards.tsx
@@ -0,0 +1,19 @@
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
+
+const ProjectCards = () => {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardFooter>
+
+        </CardFooter>
+      </Card>
+    </>
+  )
+}
+
+export default ProjectCards
--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,7 +1,26 @@
+import ProjectCards from "./ProjectCards";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
+
 const ProjectSection = () => {
   return (
-    <section>
-      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">Projects</h2>
+    <section className="space-y-6">
+      <h2 className="scroll-m-20 border-b pb-2 text-xl font-semibold tracking-tight first:mt-0">Projects</h2>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem className="lg:basis-1/2">
+            <ProjectCards />
+          </CarouselItem>
+          <CarouselItem className="lg:basis-1/2">
+            <ProjectCards />
+          </CarouselItem>
+          <CarouselItem className="lg:basis-1/2">
+            <ProjectCards />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
     </section>
   )
 }
